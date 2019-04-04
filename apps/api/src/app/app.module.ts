d9367131f1fe42f5c21461a1fd059e20e8b4c52f
Edgar Gonzalez <edgarsys@gmail.com>
2019-04-03 21:22:53 -0300

    feat: add generic error handler

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from '../shared/exception-filter/all.exceptions.filter';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter
+    },
+    AppService
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
