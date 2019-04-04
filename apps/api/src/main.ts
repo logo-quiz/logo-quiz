6af59c8059593eb26576dc74a5b0800e6a1fc266
Edgar Gonzalez <edgarsys@gmail.com>
2019-04-04 12:27:28 -0300

    chore: add missing script and set PORT env var

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`api`);
-  const port = process.env.port || 3333;
+  const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
   });
