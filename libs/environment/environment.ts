1cfa4305e6cb9b3a2631b0690bd0d2a7c74a06ad
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2020-06-25 09:00:41 -0500

    feat: add node_env console

--- libs/environment/environment.ts
+++ libs/environment/environment.ts
@@ -2,5 +2,7 @@ import dev from './dev.environment';
 import prod from './prod.environment';
 import staging from './staging.environment';
 
+console.log(process.env.NODE_ENV);
+
 export const env =
   process.env.NODE_ENV === 'prod' ? prod : process.env.NODE_ENV === 'staging' ? staging : dev;
