bb8d6dfe095c7e7a7825b355ee29bed589b05b3f
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2020-06-25 10:42:46 -0500

    chore: add database pointer to env

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,7 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { Environment } from './environment.interface';
 
-export const environment = {
-  production: false
+export const environment: Environment = {
+  production: false,
+  dbUri: 'mongodb://root:example@localhost:27017/logo-quiz'
 };
