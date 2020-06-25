6c41871def38ac4c92b29e3272afbeb2fe2a4257
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2020-06-25 08:51:39 -0500

    chore: add staging dev

--- libs/environment/environment.ts
+++ libs/environment/environment.ts
@@ -1,4 +1,6 @@
 import dev from './dev.environment';
 import prod from './prod.environment';
+import staging from './staging.environment';
 
-export const env = process.env.NODE_ENV === 'prod' ? prod : dev;
+export const env =
+  process.env.NODE_ENV === 'prod' ? prod : process.env.NODE_ENV === 'staging' ? staging : dev;
